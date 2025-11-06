2fcf1213616c24a5b6217ecddb605a6182d92db7
ActuallyHello <happy.xmas1@mail.ru>
2025-11-07 00:51:45 +0300

    auth user and roles implementations

--- internal/dto/user.go
+++ internal/dto/user.go
@@ -3,19 +3,26 @@ package dto
 import "time"
 
 type RegisterUserRequest struct {
+	Username        string `json:"username" validate:"required"`
 	Email           string `json:"email" validate:"required,email"`
 	Password        string `json:"password" validate:"required,min=3,max=50"`
 	ConfirmPassword string `json:"confirm_password" validate:"required,eqfield=Password"`
 }
 
 type LoginRequest struct {
-	Email    string `json:"email" validate:"required,email"`
+	Login    string `json:"username" validate:"required"` // could be username or email
 	Password string `json:"password" validate:"required"`
 }
 
 type UserDTO struct {
 	Email     string    `json:"email"`
 	Username  string    `json:"username"`
-	Roles     []string  `json:"roles"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+type JWT struct {
+	AccessToken      string `json:"access_token"`
+	ExpiresIn        int    `json:"expires_in"`
+	RefreshExpiresIn int    `json:"refresh_expires_in"`
+	RefreshToken     string `json:"refresh_token"`
 }
