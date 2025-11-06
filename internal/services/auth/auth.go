a403f3db03945399d7a71d74fb5651cf814b6c9c
ActuallyHello <happy.xmas1@mail.ru>
2025-11-07 00:50:47 +0300

    auth user and roles implementations

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -1,9 +1,17 @@
 package auth
 
-import "context"
+import (
+	"context"
+
+	"github.com/ActuallyHello/backendstory/internal/dto"
+)
 
 type AuthService interface {
-	RegisterUser(ctx context.Context, username, email, password string) error
-	Login(ctx context.Context, username, password string) (map[string]any, error)
+	RegisterUser(ctx context.Context, username, email, password string) (dto.JWT, error)
+	Login(ctx context.Context, username, password string) (dto.JWT, error)
+
+	GetUsers(ctx context.Context) ([]dto.UserDTO, error)
+	GetRoles(ctx context.Context) ([]string, error)
+	GetRolesByUser(ctx context.Context, username string) ([]string, error)
 	GetRolesFromToken(ctx context.Context, token string) ([]string, error)
 }
--- internal/services/auth/keycloak.go
+++ internal/services/auth/keycloak.go
@@ -2,9 +2,11 @@ package auth
 
 import (
 	"context"
+	"log/slog"
 	"time"
 
 	"github.com/ActuallyHello/backendstory/internal/config"
+	"github.com/ActuallyHello/backendstory/internal/core"
 	appError "github.com/ActuallyHello/backendstory/internal/core/errors"
 	"github.com/ActuallyHello/backendstory/internal/dto"
 	"github.com/Nerzal/gocloak/v13"
@@ -12,6 +14,7 @@ import (
 
 const (
 	keycloakAuthService = "KEYCLOAK_SERVICE_CODE"
+	guestRole           = "guest"
 )
 
 type keycloakService struct {
@@ -23,9 +26,20 @@ type keycloakService struct {
 func NewKeycloakService(cfg *config.KeycloakConfig) (*keycloakService, error) {
 	client := gocloak.NewClient(cfg.Host)
 
-	token, err := client.LoginClient(context.Background(), cfg.ClientID, cfg.ClientSecret, cfg.Realm)
+	tokenObj, err := core.Retry(
+		"Login keycloak client",
+		func() (any, error) {
+			return client.LoginClient(context.Background(), cfg.ClientID, cfg.ClientSecret, cfg.Realm)
+		},
+		core.SetMaxRetriesOpt(3),
+		core.SetMaxDelayOpt(5*time.Second),
+	)
 	if err != nil {
 		return nil, appError.NewTechnicalError(err, keycloakAuthService, err.Error())
+	}
+	token, ok := tokenObj.(*gocloak.JWT)
+	if !ok {
+		return nil, appError.NewTechnicalError(nil, keycloakAuthService, "Unexpected token type!")
 	}
 
 	return &keycloakService{
@@ -35,7 +49,7 @@ func NewKeycloakService(cfg *config.KeycloakConfig) (*keycloakService, error) {
 	}, nil
 }
 
-func (kc *keycloakService) RegisterUser(ctx context.Context, username, email, password string) error {
+func (kc *keycloakService) RegisterUser(ctx context.Context, username, email, password string) (dto.JWT, error) {
 	user := gocloak.User{
 		Username: gocloak.StringP(username),
 		Email:    gocloak.StringP(email),
@@ -44,29 +58,101 @@ func (kc *keycloakService) RegisterUser(ctx context.Context, username, email, pa
 
 	userID, err := kc.client.CreateUser(ctx, kc.token.AccessToken, kc.cfg.Realm, user)
 	if err != nil {
-		return appError.NewTechnicalError(err, keycloakAuthService, err.Error())
+		return dto.JWT{}, appError.NewTechnicalError(err, keycloakAuthService, err.Error())
 	}
 
 	err = kc.client.SetPassword(ctx, kc.token.AccessToken, userID, kc.cfg.Realm, password, false)
 	if err != nil {
-		return appError.NewTechnicalError(err, keycloakAuthService, err.Error())
+		return dto.JWT{}, appError.NewTechnicalError(err, keycloakAuthService, err.Error())
 	}
 
-	// TODO set roles
-	return nil
+	kcRole, err := kc.client.GetRealmRole(ctx, kc.token.AccessToken, kc.cfg.Realm, guestRole)
+	if err != nil {
+		slog.Error("Cannot find role!", "role", guestRole, "userID", userID)
+		return dto.JWT{}, appError.NewTechnicalError(err, keycloakAuthService, "No such role as 'guest'!")
+	}
+
+	err = kc.client.AddRealmRoleToUser(ctx, kc.token.AccessToken, kc.cfg.Realm, userID, []gocloak.Role{*kcRole})
+	if err != nil {
+		slog.Error("Cannot set role!", "role", kcRole.Name, "userID", userID)
+		return dto.JWT{}, appError.NewTechnicalError(err, keycloakAuthService, "Cannot set role guest for user")
+	}
+
+	// GET ACCESS TOKEN FOR USER AND CHECK REGISTRATION PROCESS
+	jwt, err := kc.Login(ctx, username, password)
+	if err != nil {
+		return dto.JWT{}, err
+	}
+	return jwt, nil
 }
 
-func (kc *keycloakService) Login(ctx context.Context, username, password string) (map[string]any, error) {
+func (kc *keycloakService) Login(ctx context.Context, username, password string) (dto.JWT, error) {
 	token, err := kc.client.Login(ctx, kc.cfg.ClientID, kc.cfg.ClientSecret, kc.cfg.Realm, username, password)
 	if err != nil {
+		return dto.JWT{}, appError.NewAccessError(err, keycloakAuthService, err.Error())
+	}
+	return dto.JWT{
+		AccessToken:      token.AccessToken,
+		RefreshToken:     token.RefreshToken,
+		ExpiresIn:        token.ExpiresIn,
+		RefreshExpiresIn: token.RefreshExpiresIn,
+	}, nil
+}
+
+func (kc *keycloakService) RefreshToken(ctx context.Context, refreshToken string) (dto.JWT, error) {
+	token, err := kc.client.RefreshToken(ctx, refreshToken, kc.cfg.ClientID, kc.cfg.ClientSecret, kc.cfg.Realm)
+	if err != nil {
+		return dto.JWT{}, appError.NewAccessError(err, keycloakAuthService, err.Error())
+	}
+	return dto.JWT{
+		AccessToken:      token.AccessToken,
+		RefreshToken:     token.RefreshToken,
+		ExpiresIn:        token.ExpiresIn,
+		RefreshExpiresIn: token.RefreshExpiresIn,
+	}, nil
+}
+
+func (kc *keycloakService) GetRoles(ctx context.Context) ([]string, error) {
+	params := gocloak.GetRoleParams{}
+	kcRoles, err := kc.client.GetRealmRoles(ctx, kc.token.AccessToken, kc.cfg.Realm, params)
+	if err != nil {
+		return nil, appError.NewTechnicalError(err, keycloakAuthService, "No roles for current realm!")
+	}
+	roles := make([]string, 0, len(kcRoles))
+	for _, kcRole := range kcRoles {
+		roles = append(roles, *kcRole.Name)
+	}
+	return roles, nil
+}
+
+func (kc *keycloakService) GetRolesByUser(ctx context.Context, username string) ([]string, error) {
+	params := gocloak.GetUsersParams{
+		Username: &username,
+	}
+	// always return 1 element
+	kcUsers, err := kc.client.GetUsers(ctx, kc.token.AccessToken, kc.cfg.Realm, params)
+	if err != nil {
+		slog.Error("Couldn't find user by", "username", username, "err", err)
 		return nil, appError.NewTechnicalError(err, keycloakAuthService, err.Error())
 	}
-	return map[string]any{
-		"token":              token.AccessToken,
-		"refresh_token":      token.RefreshToken,
-		"expires_in":         token.ExpiresIn,
-		"refresh_expires_in": token.RefreshExpiresIn,
-	}, nil
+	if len(kcUsers) == 0 {
+		slog.Error("User doesn't exist!", "username", username)
+		return nil, appError.NewLogicalError(err, keycloakAuthService, "Couldn't find user by this username")
+	}
+
+	kcUser := kcUsers[0]
+	kcRoles, err := kc.client.GetRealmRolesByUserID(ctx, kc.token.AccessToken, kc.cfg.Realm, *kcUser.ID)
+	if err != nil {
+		slog.Error("Couldn't find user roles", "username", username, "err", err)
+		return nil, appError.NewTechnicalError(err, keycloakAuthService, err.Error())
+	}
+
+	roles := make([]string, 0, len(kcRoles))
+	for _, kcRole := range kcRoles {
+		roles = append(roles, *kcRole.Name)
+	}
+
+	return roles, nil
 }
 
 func (kc *keycloakService) GetRolesFromToken(ctx context.Context, token string) ([]string, error) {
@@ -108,8 +194,9 @@ func (kc *keycloakService) GetRolesFromToken(ctx context.Context, token string) 
 	return roles, nil
 }
 
-func (kc *keycloakService) GetUsers(ctx context.Context, token string) ([]dto.UserDTO, error) {
-	kcUsers, err := kc.client.GetUsers(ctx, token, kc.cfg.Realm, gocloak.GetUsersParams{})
+func (kc *keycloakService) GetUsers(ctx context.Context) ([]dto.UserDTO, error) {
+	params := gocloak.GetUsersParams{}
+	kcUsers, err := kc.client.GetUsers(ctx, kc.token.AccessToken, kc.cfg.Realm, params)
 	if err != nil {
 		return nil, appError.NewTechnicalError(err, keycloakAuthService, "Couldn't find any users in current realm!")
 	}
@@ -120,7 +207,6 @@ func (kc *keycloakService) GetUsers(ctx context.Context, token string) ([]dto.Us
 			Email:     *kcUser.Email,
 			Username:  *kcUser.Username,
 			CreatedAt: time.Unix(0, *kcUser.CreatedTimestamp),
-			Roles:     *kcUser.RealmRoles,
 		})
 	}
 
