da516dd9fd25ab61cc2777d74c9a41b5562370a3
ActuallyHello <happy.xmas1@mail.ru>
2025-11-08 00:32:23 +0300

    update .gitignore for vscode and local tests

--- internal/store/repositories/common/pagination.go
+++
@@ -1,9 +0,0 @@
-package common
-
-type PaginatedResult[T any] struct {
-	Data       []T   `json:"data"`
-	TotalCount int64 `json:"totalCount"`
-	Limit      int   `json:"limit"`
-	Offset     int   `json:"offset"`
-	HasMore    bool  `json:"hasMore"`
-}
