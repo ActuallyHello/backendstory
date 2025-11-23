7d7345f02a6ce0606d710b2accfd4f53944cb04e
ActuallyHello <happy.xmas1@mail.ru>
2025-11-23 18:32:25 +0300

    convert json input to db format

--- internal/store/repositories/common/search_criteria.go
+++ internal/store/repositories/common/search_criteria.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"log/slog"
+	"strings"
 
 	"github.com/ActuallyHello/backendstory/internal/dto"
 	"gorm.io/gorm"
@@ -43,25 +44,31 @@ func applyOrdering(orderBy *string) func(db *gorm.DB) *gorm.DB {
 }
 
 func applyCondition(queryCtx *gorm.DB, condition dto.SearchCondition) *gorm.DB {
+	field := convertToDBField(condition.Field)
 	switch condition.Operation {
 	case dto.OpEqual:
-		return queryCtx.Where(condition.Field+" = ?", condition.Value)
+		return queryCtx.Where(field+" = ?", condition.Value)
 	case dto.OpNotEqual:
-		return queryCtx.Where(condition.Field+" != ?", condition.Value)
+		return queryCtx.Where(field+" != ?", condition.Value)
 	case dto.OpGreater:
-		return queryCtx.Where(condition.Field+" > ?", condition.Value)
+		return queryCtx.Where(field+" > ?", condition.Value)
 	case dto.OpGreaterEq:
-		return queryCtx.Where(condition.Field+" >= ?", condition.Value)
+		return queryCtx.Where(field+" >= ?", condition.Value)
 	case dto.OpLess:
-		return queryCtx.Where(condition.Field+" < ?", condition.Value)
+		return queryCtx.Where(field+" < ?", condition.Value)
 	case dto.OpLessEq:
-		return queryCtx.Where(condition.Field+" <= ?", condition.Value)
+		return queryCtx.Where(field+" <= ?", condition.Value)
 	case dto.OpIn:
-		return queryCtx.Where(condition.Field+" in ?", condition.Value)
+		return queryCtx.Where(field+" in ?", condition.Value)
 	case dto.OpLike:
-		return queryCtx.Where(condition.Field+" like ?", condition.Value)
+		return queryCtx.Where(field+" like ?", condition.Value)
 	default:
 		slog.Warn("Undefined operation for search criteria", "operation", condition.Operation)
 		return queryCtx
 	}
+}
+
+func convertToDBField(input string) string {
+	input = strings.ReplaceAll(input, "_", "")
+	return strings.ToUpper(input)
 }
